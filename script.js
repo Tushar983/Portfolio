5de97b2237b3dec7fe3eee9684338f70a1928415
Tushar983 <tusharrna@gmail.com>
2026-02-26 16:13:37 +0530

    image was overlapping the hero section

--- script.js
+++ script.js
@@ -67,3 +67,14 @@ function animate() {
 }
 
 animate();
+window.addEventListener("scroll", function() {
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+        navbar.style.background = "#0f172a";
+        navbar.style.boxShadow = "0 4px 20px rgba(0,0,0,0.3)";
+    } else {
+        navbar.style.background = "transparent";
+        navbar.style.boxShadow = "none";
+    }
+});
