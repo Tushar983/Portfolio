a20617d8b8db2caa939daecd52e7386db0d0171a
Tushar983 <tusharrna@gmail.com>
2026-03-06 20:32:35 +0530

    added navbar style

--- script.js
+++ script.js
@@ -81,4 +81,16 @@ window.addEventListener("scroll", function() {
     }
 });
 
+window.addEventListener("scroll", function () {
+
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+
+});
+
 
