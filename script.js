29c69df57fb6111efc1f387189dd4146a8b94ac8
Tushar983 <tusharrna@gmail.com>
2026-03-06 21:12:05 +0530

    js issue

--- script.js
+++ script.js
@@ -69,17 +69,7 @@ function animate() {
 }
 
 animate();
-window.addEventListener("scroll", function() {
-    const navbar = document.querySelector(".navbar");
 
-    if (window.scrollY > 50) {
-        navbar.style.background = "#0f172a";
-        navbar.style.boxShadow = "0 4px 20px rgba(0,0,0,0.3)";
-    } else {
-        navbar.style.background = "transparent";
-        navbar.style.boxShadow = "none";
-    }
-});
 
 window.addEventListener("scroll", function () {
 
